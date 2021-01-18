3fcea3d3ab15ae0d05a4d27d1cb13bb150ebe2a9
redkaio <chrno.92@gmail.com>
2021-01-18 12:35:58 -0300

    correcao de bugs envolvendo o this; modificacao no componente App.js, fazendo com que ele agora seja um class component; atualizacao do componente App para que ele consiga pegar o valor do termo pesquisado no compononente SearchBar.js para ser pesquisado e posteriormente ser utilizado com APIs

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,19 +3,23 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
    
     
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={ this.onFormSubmit } className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input 
                             type="text" 
                             placeholder="Search a name" 
-                            name={ this.state.term } 
-                            onChange={ (e) => this.setState({ term: e.target.value }) } 
+                            value={ this.state.term } 
+                            onChange={ e => this.setState({ term: e.target.value }) } 
                         />
                     </div>
                 </form>
